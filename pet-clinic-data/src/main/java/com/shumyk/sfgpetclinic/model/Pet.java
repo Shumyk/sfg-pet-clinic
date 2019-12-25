806ddee5eb227e56ccc367e48b74b98b0dd05067
dmytro.shumyk <dmytro.shumyk@everymatrix.com>
2019-12-25 19:22:59 +0200

    Add base entity and inherit it to other models. Closes #10. [58]

--- pet-clinic-data/src/main/java/com/shumyk/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/shumyk/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.shumyk.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
