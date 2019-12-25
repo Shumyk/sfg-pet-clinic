6060bca0f9d453dabab35205ef9efce48270d498
dmytro.shumyk <dmytro.shumyk@everymatrix.com>
2019-12-25 21:10:22 +0200

    Add controller and template for Owner index page. Changed mapping on Vet controller. Closes #13 [74]

--- pet-clinic-web/src/main/java/com/shumyk/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/shumyk/sfgpetclinic/controller/OwnerController.java
@@ -3,12 +3,12 @@ package com.shumyk.sfgpetclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
-        return "vet/index";
+    public String listOwners() {
+        return "owners/index";
     }
 }
