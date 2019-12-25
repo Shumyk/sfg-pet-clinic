b68c404b1edeea00a400cc9856eb1a35a2ae55b1
dmytro.shumyk <dmytro.shumyk@everymatrix.com>
2019-12-25 19:16:15 +0200

    Implement Interfaces for Services. Closes #5. [57]

---
+++ pet-clinic-data/src/main/java/com/shumyk/sfgpetclinic/service/OwnerService.java
@@ -0,0 +1,16 @@
+package com.shumyk.sfgpetclinic.service;
+
+import com.shumyk.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
