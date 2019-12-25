c9dc2e62ce26b0c00e8e1ac2a5fe1d3f5d917197
dmytro.shumyk <dmytro.shumyk@everymatrix.com>
2019-12-25 20:09:56 +0200

    Refactored Service Interfaces to Common Base Interface. Closes #11. [68]

--- pet-clinic-data/src/main/java/com/shumyk/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/shumyk/sfgpetclinic/service/VetService.java
@@ -2,13 +2,5 @@ package com.shumyk.sfgpetclinic.service;
 
 import com.shumyk.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet pet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
