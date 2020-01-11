6dc1956c3e95c20537dcd96a86e0358efd389b00
shumyk <dima.shumyk@gmail.com>
2020-01-11 17:52:04 +0200

    Add Vets List and update Vet Controller. Closes #15. [90]

--- pet-clinic-web/src/main/java/com/shumyk/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/shumyk/sfgpetclinic/controller/VetController.java
@@ -1,14 +1,23 @@
 package com.shumyk.sfgpetclinic.controller;
 
+import com.shumyk.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(final Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
