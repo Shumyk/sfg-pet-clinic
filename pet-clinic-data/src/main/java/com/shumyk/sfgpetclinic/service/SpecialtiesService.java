a53bdc198caabd3ae067cd6a579aa5521a96a6a9
dmytro.shumyk <dmytro.shumyk@everymatrix.com>
2020-01-13 08:37:33 +0200

    Added Specialties Map Service. Closes #31 [131]

---
+++ pet-clinic-data/src/main/java/com/shumyk/sfgpetclinic/service/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.shumyk.sfgpetclinic.service;
+
+import com.shumyk.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/shumyk/sfgpetclinic/service/map/SpecialtiesMapService.java
@@ -0,0 +1,36 @@
+package com.shumyk.sfgpetclinic.service.map;
+
+import com.shumyk.sfgpetclinic.model.Speciality;
+import com.shumyk.sfgpetclinic.service.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
