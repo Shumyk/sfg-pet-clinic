70e874029b0f42c9b23b8b0f07728f7c04841236
Shumyk <dima.shumyk@gmail.com>
2020-05-06 20:35:55 +0300

    [245] Added JSON Vets endpoint. Closes #68

--- pet-clinic-web/src/main/java/com/shumyk/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/shumyk/sfgpetclinic/controller/VetController.java
@@ -1,9 +1,14 @@
 package com.shumyk.sfgpetclinic.controller;
 
+import com.shumyk.sfgpetclinic.model.Vet;
 import com.shumyk.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @RequestMapping("/vets")
 @Controller
@@ -19,5 +24,10 @@ public class VetController {
     public String listVets(final Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vet/index";
+    }
+
+    @GetMapping("/api")
+    public @ResponseBody Set<Vet> vetsJson() {
+        return vetService.findAll();
     }
 }
